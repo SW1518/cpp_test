ce37027375c8576ff3ee5e81c976a483c1593b83
SW1518 <shawn.wan14@gmail.com>
2021-01-17 19:32:47 +0000

    [cpp_test] Initial commit

---
+++ hello.cpp
@@ -0,0 +1,80 @@
+//#pragma once
+#include "hello.h"
+#include <memory>
+class Student {
+	private:
+		char name;
+		int id;
+	public:
+		Student();
+		Student& operator=(const Student&) = delete;
+		virtual void print_info();
+};
+
+void Student::print_info(){
+	std::cout<<"Name is:"<<Student::name;
+	std::cout<<"Id is:"<<Student::id;
+}
+
+Student::Student(){
+	std::cout<<"base object created"<<std::endl;
+}
+
+class Uni_student : public Student{
+	private:
+		int uni_id;
+	public:
+		Uni_student();
+		virtual void print_info();
+};
+
+Uni_student::Uni_student(){
+	std::cout<<"derived object created"<<std::endl;
+}
+void Uni_student::print_info(){
+	std::cout<<"The uni id is:"<<Uni_student::uni_id<<std::endl;
+}
+
+int a = { 2 };
+int &b = a;
+auto c = b;
+decltype(auto)d = b;
+std::unordered_map<std::string, int> m;
+template<typename T>
+inline T const& Max (T const& a, T const& b) noexcept 
+{
+	return a > b ? a : b;
+}
+
+enum class Colour {red, green};
+
+int main() {
+	Student tom;
+	Student *ptr_s {nullptr};
+	ptr_s = &tom;
+	std::unique_ptr<Student> base_s = std::make_unique<Uni_student>();
+	base_s->print_info();
+	base_s->Student::print_info();
+	a = { 5 };
+	b = { 3 };
+	std::swap(a,b);
+	auto n = m;
+	auto larger_num {Max(a,b)};
+	auto d = static_cast<float>(larger_num); 
+	Colour col;
+	std::cout<<"colour is:"<<static_cast<int>(col)<<std::endl;
+	std::nullptr_t ptr;
+	if (nullptr == 0){
+		std::cout<<"nullprt = 0"<<std::endl;
+	}
+	//std::cout<<ptr<<std::endl;
+	std::cout<<"d is:"<<d<<std::endl;
+	std::cout<<larger_num<<std::endl;
+	//decltype(b);
+	std::cout << b << std::endl;
+	std::cout << &b << std::endl;
+	std::cout << &a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << typeid(b).name() << std::endl;
+	return 0;
+}
