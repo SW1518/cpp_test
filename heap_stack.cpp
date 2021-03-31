3945b825157ba27f00847e4446b77c8b6c26414f
SW1518 <shawn.wan14@gmail.com>
2021-03-31 21:06:27 +0100

    [cpp_test] Some reference common errors

--- heap_stack.cpp
+++ heap_stack.cpp
@@ -12,6 +12,8 @@ main()
     int num {100};
     int addr {200};
     int *pInt = reinterpret_cast<int*>(&addr);
+    //int alpha = reinterpret_cast<int>(&addr); // wrong type
+    int alpha = reinterpret_cast<int>(addr);// new address
     *pInt = num;
     int b; //栈
     char s[] = "abc"; //栈
@@ -22,4 +24,5 @@ main()
     p2 = (char *)malloc(20);
     //分配得来得10和20字节的区域就在堆区。
     strcpy(p1, "123456"); //123456放在常量区，编译器可能会将它与p3所指向的"123456"优化成一个地方。
+    int &beta = num;
 }
