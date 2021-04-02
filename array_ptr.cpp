828832e35e676852cb2465eab5cc4ebd0c488168
Shun Wan <45769064+SW1518@users.noreply.github.com>
2021-04-02 16:46:59 +0100

    [cpp_test] Add stdout function

--- array_ptr.cpp
+++ array_ptr.cpp
@@ -5,6 +5,8 @@ int main(int argc, char *argv[])
     int arr[2][3] {1,2,3,
                    4,5,6};
     int (*p)[3] = arr;
+    std::cout<<**(p+1)<<std::endl;
+    std::cout<<**p+1<<std::endl;
     std::cout<<*(*(p+1))<<std::endl;
     std::cout<<*(*p+1)<<std::endl;
 
